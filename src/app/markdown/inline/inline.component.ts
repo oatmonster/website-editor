1043b5d78580773e152642122eb45cfc5cb96fa5
Alex Zhao <oatmonster@gmail.com>
2020-07-06 16:16:07 -0700

    Simplified image urls

--- src/app/markdown/inline/inline.component.ts
+++ src/app/markdown/inline/inline.component.ts
@@ -28,8 +28,7 @@ export class MarkdownInlineComponent {
     return false;
   }
 
-  public imageUrl( image: string, width: 1080 ): string {
-    let info = image.split( '.' );
-    return environment.imageUrl + '/' + this.tree.type + '/' + this.tree.id + '/' + info[ 0 ] + '_' + width + 'w.' + info[ 1 ];
+  public imageUrl( url: string ): string {
+    return environment.imageUrl + '/' + this.tree.type + '/' + this.tree.id + '/' + url;
   }
 }
