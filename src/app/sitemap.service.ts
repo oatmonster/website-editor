c7f6c623f0193cb2a6df644dd0928a1db2b36fc6
Alex Zhao <oatmonster@gmail.com>
2020-07-16 01:40:17 -0700

    Added images to sitemap

--- src/app/sitemap.service.ts
+++ src/app/sitemap.service.ts
@@ -12,16 +12,16 @@ export class SitemapService {
   public async getSitemap(): Promise<string> {
     let blogPageSize = 10;
     let blogPageCount: number;
-    let blogPosts: Array<string>;
+    let blogPosts: Array<IBlogPost>;
 
     let res = await this.apiService.getBlogPosts().pipe( first() ).toPromise();
 
     blogPageCount = Math.ceil( res.count / blogPageSize );
 
-    // Get array of public blog post IDs
+    // Get array of public blog post
     blogPosts = res.posts.filter( post => {
       return post.public !== false;
-    } ).map( post => { return post.id } );
+    } );
 
     // Get the posts from the latter pages
     let promises = [];
@@ -34,19 +34,50 @@ export class SitemapService {
       values.map( res => {
         blogPosts = blogPosts.concat( res.posts.filter( post => {
           return post.public !== false;
-        } ).map( post => { return post.id } ) );
+        } ) );
       } );
     } );
 
     // Generate the sitemap
-    let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    let sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+    sitemap += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">'
     sitemap += '\n\t<url>\n\t\t<loc>https://www.alexzhao.me</loc>\n\t\t<changefreq>monthly</changefreq>\n\t</url>';
     sitemap += '\n\t<url>\n\t\t<loc>https://www.alexzhao.me/blog</loc>\n\t\t<changefreq>daily</changefreq>\n\t</url>';
     sitemap += '\n\t<url>\n\t\t<loc>https://www.alexzhao.me/projects</loc>\n\t\t<changefreq>monthly</changefreq>\n\t</url>';
     sitemap += '\n\t<url>\n\t\t<loc>https://www.alexzhao.me/about</loc>\n\t\t<changefreq>monthly</changefreq>\n\t</url>';
 
     blogPosts.forEach( post => {
-      sitemap += `\n\t<url>\n\t\t<loc>https://www.alexzhao.me/blog/${post}</loc>\n\t\t<changefreq>weekly</changefreq>\n\t</url>`;
+
+      post.subtitle = post.subtitle.replace( /'/g, '&apos;' );
+      post.subtitle = post.subtitle.replace( /"/g, '&quot;' );
+
+      sitemap += `\n\t<url>\n\t\t<loc>https://www.alexzhao.me/blog/${post.id}</loc>\n\t\t<changefreq>weekly</changefreq>`;
+
+      // Thumbnail
+      sitemap += '\n\t\t<image:image>'
+      sitemap += `\n\t\t\t<image:loc>https://www.alexzhao.me/blog/${post.id}/thumbnail_1920w.jpg</image:loc>`;
+      sitemap += `\n\t\t\t<image:title>${post.title} | Alex Zhao</image:title>`;
+      sitemap += `\n\t\t\t<image:caption>${post.subtitle || ''}</image:caption>`;
+      sitemap += '\n\t\t</image:image>'
+
+
+      // Get images from content
+      let regex = /!\[(.*)]\((\w*\.*\w*)(?:\ "(.*)")?\)/g;
+      let images = [ ...post.content.matchAll( regex ) ];
+
+      images.forEach( image => {
+        image.forEach( ( e, i, a ) => {
+          a[ i ] = a[ i ].replace( /'/g, '&apos;' );
+          a[ i ] = a[ i ].replace( /"/g, '&quot;' );
+        } );
+        sitemap += '\n\t\t<image:image>'
+        sitemap += `\n\t\t\t<image:loc>https://www.alexzhao.me/images/blog/${image[ 2 ]}</image:loc>`;
+        sitemap += `\n\t\t\t<image:title>${image[ 3 ] || ''} | Alex Zhao</image:title>`;
+        sitemap += `\n\t\t\t<image:caption>${image[ 2 ] || ''}</image:caption>`;
+        sitemap += '\n\t\t</image:image>'
+      } );
+
+      sitemap += '\n\t</url>';
     } );
 
     // Add extra blog pages
@@ -56,7 +87,6 @@ export class SitemapService {
     }
 
     sitemap += '\n</urlset>';
-    console.log( blogPosts );
 
     return sitemap;
   }
